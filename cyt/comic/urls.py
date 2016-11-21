d44cd219c0db2d4d128c2e7d4f4a73b7ac96e81b
David McLintock <damclintock@gmail.com>
2016-11-21 17:19:25 +0000

    Moved uploaded files to media server

--- cyt/comic/urls.py
+++ cyt/comic/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about')
-]
+] 
--- cyt/cyt/settings.py
+++ cyt/cyt/settings.py
@@ -117,5 +117,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_ROOT = 'mediafiles'
+MEDIA_URL = '/media/'
+
+MEDIAFILES_DIRS = (
+    os.path.join(BASE_DIR, 'comic/media'),
+)
