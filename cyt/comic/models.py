2063da1549b4977f8c3df7a4f6b7c0ff5f2545df
David McLintock <damclintock@gmail.com>
2016-11-22 19:39:43 +0000

    Dates now added automatically

--- cyt/comic/models.py
+++ cyt/comic/models.py
@@ -7,9 +7,9 @@ class Strip(models.Model):
     image = models.ImageField()
     alt = models.CharField(max_length=500)
     subtext = models.CharField(max_length=100000)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateField(auto_now_add=True)
 
 class Post(models.Model):
     title = models.CharField(max_length=200, default="")
     text = models.CharField(max_length=100000, default="")
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateField(auto_now_add=True)
--- cyt/comic/urls.py
+++ cyt/comic/urls.py
@@ -5,4 +5,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about')
-] 
+]
