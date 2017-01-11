d3aec053b8dde70e7f83049151465669cee24a25
David McLintock <damclintock@gmail.com>
2017-01-11 13:59:15 +0000

    Added pagination and test images

--- cyt/comic/views.py
+++ cyt/comic/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Post, Strip
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # index page
 def index(request):
-    current_strip = Strip.objects.latest('pub_date')
+    all_strips = Strip.objects.all()
+    paginator = Paginator(all_strips, 1)
+
+    page = request.GET.get('page')
+    try:
+        current_strip = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        current_strip = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        current_strip = paginator.page(paginator.num_pages)
+
     recent_posts = Post.objects.order_by('pub_date')[:5]
     context_dict = {'current_strip': current_strip,
                     'recent_posts': recent_posts}
--- cyt/cyt/urls.py
+++ cyt/cyt/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^comic/', include('comic.urls'))
+    url(r'^', include('comic.urls'))
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
